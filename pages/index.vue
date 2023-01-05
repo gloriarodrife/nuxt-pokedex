587c91f8b5253a336950cfe3193a0868201311d2
gloriarodrife <gloriarodrife@gmail.com>
2023-01-05 18:31:12 -0500

    Create initial pages

---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>Home</div>
+  <NuxtLink to="/pokemon/001"> Pokemon 001</NuxtLink>
+</template>
+ 
---
+++ pages/pokemon/[id].vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Pokemon {{ pokemonId }}</div>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute();
+
+const pokemonId = route.params.id;
+</script>
