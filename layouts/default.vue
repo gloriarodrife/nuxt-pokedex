14c050b2eb6ab9ad4c2b03a7a6ca88b03605915d
gloriarodrife <gloriarodrife@gmail.com>
2023-01-08 19:47:02 -0500

    Add header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,16 @@
 <template>
+  <header>
+    <Header />
+  </header>
+
   <slot />
 </template>
+
+<style>
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+}
+</style>
