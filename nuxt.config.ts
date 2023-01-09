dd0da8422b939cf025ced444a8ffa511a08121e9
gloriarodrife <gloriarodrife@gmail.com>
2023-01-08 19:18:55 -0500

    Add global config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  runtimeConfig: {
+    public: {
+      API_BASE_URL:
+        'https://q-exercise-api.o64ixruq9hj.us-south.codeengine.appdomain.cloud/api/rest',
+    },
+  },
+});
