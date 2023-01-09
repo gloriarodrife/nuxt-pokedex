9e4a71ba0b80cb7b250e6616df6fa9f5003397d3
gloriarodrife <gloriarodrife@gmail.com>
2023-01-08 20:48:10 -0500

    Add pokemon list

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>Home</div>
-  <NuxtLink to="/pokemon/001"> Pokemon 001</NuxtLink>
+  <div class="list">
+    <NuxtLink v-for="pokemon in pokemons?.items" :to="`/pokemon/${pokemon.id}`">
+      <PokemonCard :pokemon="pokemon" />
+    </NuxtLink>
+  </div>
 </template>
- 
+
+<script lang="ts" setup>
+import type { PokemonsApiResponse } from '~/types';
+
+const config = useRuntimeConfig();
+
+const { pending, data: pokemons } = await useLazyFetch<PokemonsApiResponse>(
+  `${config.public.API_BASE_URL}/pokemon`
+);
+</script>
+
+<style lang="scss">
+.list {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(160px, 1fr));
+  gap: 1rem;
+  justify-items: center;
+  list-style: none;
+  margin: 0;
+  padding: 1rem;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+}
+</style>
