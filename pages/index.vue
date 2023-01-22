f1209452ebf55ea3f01b7ddbd7b069684166772f
gloriarodrife <gloriarodrife@gmail.com>
2023-01-22 12:00:42 -0500

    Feat: Load more pokemons.

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,48 @@
 <template>
   <ul class="list" v-if="pokemons">
-    <li v-for="pokemon in pokemons.items" :key="pokemon.id">
+    <li v-for="pokemon in pokemons" :key="pokemon.id">
       <NuxtLink :to="`/pokemon/${pokemon.id}`">
         <PokemonCard :pokemon="pokemon" />
       </NuxtLink>
     </li>
   </ul>
+
+  <button @click="loadMorePokemons">More pokemons</button>
 </template>
 
 <script lang="ts" setup>
+import type { ParamsPokemons, PokemonsApiResponse } from '~/types';
 import { getPokemons } from '~/services/pokemons';
+
+const pokemonsCache = useState<PokemonsApiResponse['items']>(() => []);
+
+const queryParams = ref<ParamsPokemons>({
+  limit: 4,
+  offset: 0,
+  search: '',
+  isFavorite: false,
+  type: '',
+});
 
 const {
   pending,
   data: pokemons,
   refresh,
 } = await useLazyAsyncData('pokemon-list', async () => {
-  const response = await getPokemons({ limit: 20, offset: 0 });
-  return response;
+  const response = await getPokemons({
+    limit: queryParams.value.limit,
+    offset: queryParams.value.offset,
+  });
+
+  pokemonsCache.value = [...pokemonsCache.value, ...response.items];
+
+  return pokemonsCache.value;
 });
+
+const loadMorePokemons = () => {
+  queryParams.value.offset += queryParams.value.limit;
+  refresh();
+};
 </script>
 
 <style lang="scss">
